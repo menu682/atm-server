873aa38f87b72f0b2a43d8e3a41f5bb9fdf1c6fc
Fenix <menu682@gmail.com>
2021-12-12 14:05:40 +0200

    complete

--- src/main/java/ua/lomakin/atm/entity/BaseEntity.java
+++ src/main/java/ua/lomakin/atm/entity/BaseEntity.java
@@ -1,15 +1,10 @@
 package ua.lomakin.atm.entity;
 
 import lombok.Data;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.Column;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
